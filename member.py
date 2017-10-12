a431af37d099019932b45f1cab0cec5ca7104836
Catherine Mangeym <mangcat390@gmail.com>
2017-10-12 15:23:15 -0400

    Member data model

--- member.py
+++ member.py
@@ -2,24 +2,16 @@ class Member(models.Model):
     """
     Model representing a member.
     """
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    sm_links = 
-    stock = models.ManyToManyField('Stock', help_text="Select a stock for this member")
-    post = models.ManyToManyField('Post', help_text="Select a post for this member")
+    name = models.CharField(max_length=100)
+    sm_links = models.URLField() 
+    stock = models.OnetoOneField('Stock', help_text="Select a stock for this member")
+    post = models.ForeignKey('Post', help_text="Select a post for this member")
 
 
     def __str__(self):
         """
         String for representing the Model object.
         """
-        return '%s, %s' % (self.last_name, self.first_name)
-    
-    
-    def get_social_media_link(self):
-        """
-        Returns the link to a particular social media.
-        """
-        return reverse('author-detail', args=[str(self.id)])
+        return '%s' % (self.name)
     
 
