a78791d332ce81dff8eaefac820ac494ef424f4c
RaymondLeo <rayleo20@gmail.com>
2017-10-24 18:43:50 -0400

    more home screen work

--- anonymous/flexfeed/admin.py
+++ anonymous/flexfeed/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post, Stock, Category, Member, Group, User
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
--- anonymous/flexfeed/models.py
+++ anonymous/flexfeed/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     site = models.CharField(max_length=50, help_text="The social media site this post was made on.")
     text = models.CharField(max_length=500, help_text="The text in the social media post.")
     #pic = models.ImageField(upload_to="../pics/post_pics", help_text="The picture in the social media post.")
-    picture = models.URLField()
+    picture = models.URLField(max_length=20000)
 
 
     def __str__(self):
@@ -23,14 +23,14 @@ class Stock(models.Model):
     Model representing a book genre (e.g. Science Fiction, Non Fiction).
     """
     company = models.CharField(max_length=50, help_text="The company that has this stock value.")
-    value = models.BigIntegerField(help_text="The value of the stock.")
+    value = models.DecimalField(help_text="The value of the stock.",max_digits=5,decimal_places=2)
     trend = models.IntegerField(help_text="0 if trending down, 1 if trending up.")
 
     def __str__(self):
         """
         String for representing the Model object (in Admin site etc.)
         """
-        return self.company + " " + self.value + " " + self.trend
+        return self.company + " " + str(self.value) + " " + str(self.trend)
 
 
 class Category(models.Model):
@@ -88,9 +88,9 @@ class User(models.Model):
 
     # fields
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular user")
-    first_name = models.TextField(help_text="Enter your first name")
-    last_name = models.TextField(help_text="Enter your last name")
-    username = models.CharField(max_length=15, help_text="Enter your username (max 15 characters): ")
+    first_name = models.CharField(max_length=20, help_text="Enter your first name")
+    last_name = models.CharField(max_length=20, help_text="Enter your last name")
+    username = models.CharField(max_length=25, help_text="Enter your username (max 15 characters): ")
     email = models.EmailField()
     password = models.CharField(max_length=30, help_text="Enter your password: ")
     #profile_picture = models.ImageField(upload_to = '../pics/user_pics/', default = '../pics/user_pics/no-img.jpg',
@@ -100,4 +100,3 @@ class User(models.Model):
 
     def __str__(self):
         return '%s %s' % (self.first_name, self.last_name)
-
