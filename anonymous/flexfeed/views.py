74478f4238edf872f76e7b7188387afd6669fe08
RaymondLeo <rayleo20@gmail.com>
2017-10-30 12:53:15 -0400

    turned groups.html into a template

--- anonymous/flexfeed/views.py
+++ anonymous/flexfeed/views.py
@@ -9,11 +9,22 @@ def index(request):
     all_Groups=Group.objects.all()
     IG_Posts =Post.objects.filter(site__contains='IG')
     TWT_Posts=Post.objects.filter(site__contains='TWT')
-    FB_Posts =Post.objects.filter(site__contains='FB')
+    FB_Posts = Post.objects.filter(site__contains='FB')
   
     return render(
         request,
         'home.html',
-        context={'all_Stocks':all_Stocks,'all_Users':all_Users,'all_Groups':all_Groups, 'IG_Posts':IG_Posts,
-        'TWT_Posts':TWT_Posts,'FB_Posts':FB_Posts},
+        context={'all_Stocks': all_Stocks, 'all_Users': all_Users, 'all_Groups': all_Groups, 'IG_Posts': IG_Posts,
+                 'TWT_Posts': TWT_Posts, 'FB_Posts': FB_Posts},
+    )
+
+
+def groups(request):
+
+    all_groups = Group.objects.all()
+
+    return render(
+        request,
+        'groups.html',
+        context={'all_groups': all_groups}
     )
