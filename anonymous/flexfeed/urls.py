9490912d753c4c877df7d714694b0418f1153462
Sebastian Lacki <sebastianlacki@Sebastians-MacBook-Pro.local>
2017-10-31 03:42:14 -0400

    added templates for everyone to follow

--- anonymous/flexfeed/urls.py
+++ anonymous/flexfeed/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^groups/$', views.groups, name='groups'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^discovery/$', views.discover, name='discover'),
+    url(r'^groups/edit/$', views.editgroups, name='edit')
 ]
--- anonymous/flexfeed/views.py
+++ anonymous/flexfeed/views.py
@@ -3,21 +3,20 @@ from .models import Stock, User, Group, Post
 
 
 def index(request):
-  
+
     all_Stocks=Stock.objects.all()
     all_Users=User.objects.all()
     all_Groups=Group.objects.all()
     IG_Posts =Post.objects.filter(site__contains='IG')
     TWT_Posts=Post.objects.filter(site__contains='TWT')
     FB_Posts = Post.objects.filter(site__contains='FB')
-  
+
     return render(
         request,
         'home.html',
         context={'all_Stocks': all_Stocks, 'all_Users': all_Users, 'all_Groups': all_Groups, 'IG_Posts': IG_Posts,
                  'TWT_Posts': TWT_Posts, 'FB_Posts': FB_Posts},
     )
-
 
 def groups(request):
 
@@ -26,5 +25,55 @@ def groups(request):
     return render(
         request,
         'groups.html',
+        context={'all_groups': all_groups}
+    )
+
+def editgroups(request):
+
+    all_groups = Group.objects.all()
+
+    return render(
+        request,
+        'editgroups.html',
+        context={'all_groups': all_groups}
+    )
+
+def discover(request):
+
+    all_groups = Group.objects.all()
+
+    return render(
+        request,
+        'discovery.html',
+        context={'all_groups': all_groups}
+    )
+
+def login(request):
+
+    all_groups = Group.objects.all()
+
+    return render(
+        request,
+        'login.html',
+        context={'all_groups': all_groups}
+    )
+
+def temp(request):
+
+    all_Stocks=Stock.objects.all()
+
+    return render(
+        request,
+        'flexfeed_template.html',
+        context={'all_Stocks': all_Stocks},
+    )
+
+def p(request):
+
+    all_groups = Group.objects.all()
+
+    return render(
+        request,
+        'practice.html',
         context={'all_groups': all_groups}
     )
