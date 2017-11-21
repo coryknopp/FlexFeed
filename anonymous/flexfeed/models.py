93cad9eb3e79813f6b5fb1d8e86b44d9b10edcb9
Sebastian Lacki <sebastianlacki@Sebastians-MacBook-Pro.local>
2017-11-21 15:12:04 -0500

    Profile extends Users

--- anonymous/flexfeed/models.py
+++ anonymous/flexfeed/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Post(models.Model):
     """
@@ -95,8 +97,18 @@ class Profile(models.Model):
 
     # fields
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
     profile_picture = models.URLField()
     groups = models.ManyToManyField(Group, help_text="Select a group")
 
     def __str__(self):
         return '%s %s' % (self.user.first_name, self.user.last_name)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
