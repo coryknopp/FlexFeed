f2210080cd5a54b38ce0278ca27f696a6cb340e3
coryknopp <cknopp@umass.edu>
2017-11-25 20:48:29 -0500

    rendered home page view to user

--- anonymous/flexfeed/views.py
+++ anonymous/flexfeed/views.py
@@ -6,12 +6,14 @@ def index(request):
 
     all_Stocks=Stock.objects.all()
     all_groups=Media_Group.objects.all()
+    unique_Members = None
     user = None
     if request.user.is_authenticated():
         user = request.user
         all_groups = request.user.profile.media_group.all()
+        unique_Members = Member.objects.filter(media_group__in=all_groups).distinct()
 
-    
+
     IG_Posts =Post.objects.filter(site__contains='IG')
     TWT_Posts=Post.objects.filter(site__contains='TWT')
     FB_Posts = Post.objects.filter(site__contains='FB')
@@ -21,7 +23,7 @@ def index(request):
         request,
         'home.html',
         context={'all_Stocks': all_Stocks, 'user': user, 'all_Groups': all_groups, 'all_Posts':all_Posts,
-                'IG_Posts': IG_Posts, 'TWT_Posts': TWT_Posts, 'FB_Posts': FB_Posts},
+                'IG_Posts': IG_Posts, 'TWT_Posts': TWT_Posts, 'FB_Posts': FB_Posts, 'unique_Members': unique_Members},
     )
 
 
