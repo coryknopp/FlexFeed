b650201dd2e7d3e3569f51ae5740493a1f0cf2a4
Lauren Bornfeld <laurenb227@yahoo.com>
2017-11-30 13:41:11 -0500

    stock admin

--- anonymous/flexfeed/models.py
+++ anonymous/flexfeed/models.py
@@ -30,14 +30,15 @@ class Stock(models.Model):
     Model representing a book genre (e.g. Science Fiction, Non Fiction).
     """
     company = models.CharField(max_length=50, help_text="The company that has this stock value.")
+    symbol = models.CharField(max_length=50, help_text="The company's market symbol (i.e. AAPL).", default = '')
     # value = models.DecimalField(help_text="The value of the stock.",max_digits=5,decimal_places=2)
     # trend = models.IntegerField(help_text="0 if trending down, 1 if trending up.")
-    market = models.CharField(max_length=50, help_text="The market containing this company.", default = '')
+    market = models.CharField(max_length=50, help_text="The market containing this company (i.e. NASDAQ).", default = '')
     def __str__(self):
         """
         String for representing the Model object (in Admin site etc.)
         """
-        return self.company + " " + str(self.value) + " " + str(self.trend)
+        return self.company + " " + str(self.market) + " " + str(self.symbol)
 
 
 class Category(models.Model):
